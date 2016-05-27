c2af833d7534ae2873f0e6f3fd59e78e6903460b
Fatima Manso <fmanro73@gmail.com>
2016-05-27 10:54:32 +0200

    Búsquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,36 +1,49 @@
 var models = require('../models');
 
-// GET /question
-exports.question = function(req, res, next) {
-	models
-	.Quiz
-	.findOne() //Busca la primera pregunta
-	.then(function(quiz){
-	   if(quiz) {
-	        var answer = req.query.answer || '';
-		res
-		.render('quizzes/question', {question: quiz.question, answer: answer});
-	    } 
-	    else {
-		throw new Error('No hay preguntas en la BBDD.');
-	    }
-	}).catch(function(error) {next(error);});
+// GET /quizzes
+exports.index = function(req, res, next){
+  if(!req.query.search){	
+	 models.Quiz.findAll()
+	   .then(function(quizzes) {
+		res.render('quizzes/index.ejs', { quizzes: quizzes});
+   		})
+	   .catch(function(error) { next(error); });
+  }else {
+	models.Quiz.findAll({
+		where: { question: {$like: "%" + req.query.search.split(" ").join("%") + "%"} }
+	}).then(function(quizzes){
+		var busqueda = req.query.search;
+		res.render('quizzes/index', {quizzes: quizzes.sort(), busqueda: busqueda});
+	}).catch(function(error){ next(error); });
+   }
+};
+// GET /quizzes/:id
+exports.show = function(req, res, next) {
+	models.Quiz.findById(req.params.quizId)
+		.then(function(quiz){
+		   if(quiz) {
+	        	var answer = req.query.answer || '';
+			res.render('quizzes/show', {quiz: quiz, answer: answer});
+	    		} 
+		    else {
+			throw new Error('No existe ese quiz en la BBDD.');
+	             }
+	}).catch(function(error) { next(error); });
 };
 
-//GET /check
-exports.check = function(req, res, next){
-	models
-	.Quiz
-	.findOne() //Busca la primera pregunta
-	.then(function(quiz){
-	   if(quiz){ 
-		var answer =req.query.answer || "";
-		var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-		res.render('quizzes/result', {result: result,
-				       answer: answer });
+//GET /quizzes/:id/check
+exports.check = function(req, res){
+	models.Quiz.findById(req.params.quizId)
+		.then(function(quiz){
+		   if(quiz){ 
+			var answer =req.query.answer || "";
+			var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+			res.render('quizzes/result', {quiz: quiz,
+						      result: result,
+		   			              answer: answer });
 	    }
 	    else{
-		throw new Error ('No hay preguntas en la BBDD');
+		throw new Error ('No existe ese quiz en la BBDD');
 	    }
 	}).catch(function(error) { next(error); });
 };
