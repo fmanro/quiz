4f97d2b7c105b32ab740592bb7d9b83dde825c84
Fatima Manso <fmanro73@gmail.com>
2016-05-28 16:54:49 +0200

    Tema 18: Crear comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,5 +22,8 @@ router.get('/quizzes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',		quizController.update);
 router.delete('/quizzes/:quizId(\\d+)', 	quizController.destroy);
 router.get('/autor', 				quizController.autor);
+
+router.get('/quizzes/:quizId(\\d+)/comments/new', 	commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments', 		commentController.create);
 
 module.exports = router;
